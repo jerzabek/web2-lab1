8d79b6ff4ce54df6da21e701931b834a61f8c21a
Ivan Jeržabek <jerzabek.ivan@gmail.com>
2022-10-20 13:09:16 +0200

    Homepage title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return <>
+    <Head>
+      <title>HNL Rezultati</title>
+      <meta name="description" content="Rezultati HNL lige" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <Component {...pageProps} />
+  </>
 }
 
-export default MyApp
+export default App
